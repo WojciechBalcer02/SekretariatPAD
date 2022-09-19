8d0bbfc3c4abc6e851b4dd2e6e95e8c82c95087f
WojciechBalcer02 <wojciechbalcer02@gmail.com>
2022-09-19 19:24:35 +0200

    Dodaj pliki projektów.

---
+++ SekretariatPAD/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SekretariatPAD
+{
+    partial class Sekretariat
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sekretariat));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textW = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.napisZdj = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(110, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Użytkownik:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(143, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasło:";
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(252, 39);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(172, 20);
+            this.user.TabIndex = 2;
+            this.user.TextChanged += new System.EventHandler(this.user_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(252, 80);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(172, 20);
+            this.password.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(110, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Weryfikacja:";
+            // 
+            // textW
+            // 
+            this.textW.AutoSize = true;
+            this.textW.Location = new System.Drawing.Point(163, 170);
+            this.textW.Name = "textW";
+            this.textW.Size = new System.Drawing.Size(0, 13);
+            this.textW.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "zdjęcie";
+            // 
+            // napisZdj
+            // 
+            this.napisZdj.Location = new System.Drawing.Point(252, 247);
+            this.napisZdj.Name = "napisZdj";
+            this.napisZdj.Size = new System.Drawing.Size(172, 20);
+            this.napisZdj.TabIndex = 7;
+            // 
+            // login
+            // 
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.login.Location = new System.Drawing.Point(474, 340);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 8;
+            this.login.Text = "Zaloguj";
+            this.login.UseVisualStyleBackColor = true;
+            // 
+            // Sekretariat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.napisZdj);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textW);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Sekretariat";
+            this.Text = "Sekretariat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label textW;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox napisZdj;
+        private System.Windows.Forms.Button login;
+    }
+}
+
---
+++ SekretariatPAD/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SekretariatPAD
+{
+    public partial class Sekretariat : Form
+    {
+        public Sekretariat()
+        {
+            InitializeComponent();
+            textW.Text = "Wpisz znaki które widzisz na poniższym obrazku, aby kontynuować";
+        }
+
+        private void user_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
